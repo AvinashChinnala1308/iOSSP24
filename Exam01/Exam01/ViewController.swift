6d2d24c138c59da2205b70b01cb05e252ad64f31
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2024-02-21 14:13:14 -0600

    initial commit

---
+++ Exam01/Exam01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam01
+//
+//  Created by Avinash Chinnala on 2/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
